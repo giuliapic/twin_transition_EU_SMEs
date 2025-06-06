f26c52c2cef24ff4bf81f0a51bb3376d912c0216
giuliapic <giulia.picardi01@gmail.com>
2025-06-06 23:02:30 +0200

    setup

---
+++ 01_variables.R
@@ -0,0 +1,35 @@
+#libraries 
+
+library(haven)
+library(tidyverse)
+library(dplyr)
+library(sjPlot)
+
+
+#uploading the datasets for 486 (green developement)
+
+eb498_green <- read_dta("C:/data_giulia/data/eurobarometer/data_489_green.dta")
+
+
+table(eb498_green$isocntry)
+
+
+#selecting the country "italy" 
+
+eb498_green_it <- eb498_green[eb498$isocntry == "IT", ]
+eb498_green_it
+
+
+
+#test
+
+
+
+
+
+
+
+
+
+
+
